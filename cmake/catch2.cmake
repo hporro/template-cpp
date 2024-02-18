48ab7906151c3da9802dd932ceaa1dd8d9f47d53
Heinich Porro <heinich11@gmail.com>
2024-02-18 18:17:59 +0100

    Update catch2.cmake
    update catch2 version tag

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
     catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2
-    GIT_TAG v3.4.0
+    GIT_TAG v3.5.2
 )
 FetchContent_MakeAvailable(catch2)
